fb70c0191057c4103d5c6045ba049d4582d17130
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-12-28 16:04:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,3 +29,7 @@ end
 def using_insert(list_of_programming_languages, another_language)
   return list_of_programming_languages.insert(4, another_language)
 end
+
+def using_uniq(haircuts)
+  return haircuts.uniq()
+end 
