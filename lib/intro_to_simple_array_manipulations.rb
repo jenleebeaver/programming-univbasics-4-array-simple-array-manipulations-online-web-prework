1312729820aa77bfbf957e86669dd393abf3acf6
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-12-28 15:38:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,5 +27,5 @@ def using_concat(my_favorite_things, more_favs)
 end
 
 def using_insert(list_of_programming_languages, another_language)
-  return list_of_programming_languages.insert(another_language[4])
+  return list_of_programming_languages.insert(4, another_language)
 end
